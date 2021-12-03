13ca0c03d21fc423fa113aeb74acc6709cc81d7d
anhtoan260497 <anhtoan260497@gmail.com>
2021-12-03 13:02:33 +0700

    fix logic

--- to-do-app/src/Components/TodoList/index.jsx
+++ to-do-app/src/Components/TodoList/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import TodoForm from "../../Components/TodoForm";
 import TodoItems from "../TodoItems";
 import TodoOptions from "../TodoOptions";
@@ -7,7 +7,9 @@ function TodoList({ isDark }) {
   const [todo, setTodo] = useState([]);
   const [todoRender, setTodoRender] = useState(todo);
   const [status, setStatus] = useState("all");
+  const todoAll = useRef([]); // tạo Ref để giữ giá trị của mảng todo All
 
+  // Submit 
   const handleTodoFormSubmit = (values) => {
     const newTodoInput = {
       job: values.todo,
@@ -18,19 +20,29 @@ function TodoList({ isDark }) {
   };
 
   useEffect(() => {
+    todoAll.current = todo;
+  }, [todo]); // gán todo hiện tại cho Ref todo All
+
+
+  //useEffect cho việc hiển thị số lượng công việc tuỳ vào trạng thái
+  useEffect(() => {
     const newTodoClone = [...todo];
     switch (status) {
       case "all":
-        setTodoRender(todo);
+        setTodoRender(todo); 
         break;
       case "active":
-        const activeTodoList = newTodoClone.filter((item) => item.completed === false);
+        const activeTodoList = newTodoClone.filter(
+          (item) => item.completed === false
+        );
         setTodoRender(activeTodoList);
         break;
-    case 'completed':
-        const completedTodoList = newTodoClone.filter((item) => item.completed === true);
+      case "completed":
+        const completedTodoList = newTodoClone.filter(
+          (item) => item.completed === true
+        );
         setTodoRender(completedTodoList);
-        break
+        break;
       default:
         return null;
     }
@@ -38,24 +50,35 @@ function TodoList({ isDark }) {
 
   const handleClickItem = (idx) => {
     if (todo[idx].completed === "true") return;
-    const newTodoList = [...todo];
-    newTodoList[idx].completed = true;
-    setTodo(newTodoList);
+    if (status === "all") {
+      const newTodoList = [...todoAll.current];
+      newTodoList[idx].completed = true;
+      setTodo(newTodoList);
+    } else if (status === "active") {
+      const newTodoList = [...todoRender];
+      newTodoList[idx].completed = true;
+      setTodo(newTodoList);
+    }
+
+    console.log(idx);
   };
 
+  // Click nút xoá
   const handleClickButton = (idx) => {
     const newTodoList = [...todo];
     newTodoList.splice(idx, 1);
     setTodo(newTodoList);
   };
 
+  // Click Status
   const handleStatusClick = (status) => {
     setStatus(status);
   };
 
+  // Click nút Clear
   const handleClearClick = () => {
-      setTodo([])
-  }
+    setTodo([]);
+  };
 
   return (
     <div>
@@ -72,6 +95,7 @@ function TodoList({ isDark }) {
           isDark={isDark}
           handleStatusClick={handleStatusClick}
           handleClearClick={handleClearClick}
+          status={status}
         />
       ) : null}
     </div>
